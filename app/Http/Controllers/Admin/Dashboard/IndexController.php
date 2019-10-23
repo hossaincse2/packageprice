e63ac3dbc0e6338532f867324fc5f51b5216ee4d
hossain <hossaincse2@gmail.com>
2019-10-23 18:05:29 +0600

    laravel graph library install'

--- app/Http/Controllers/Admin/Dashboard/IndexController.php
+++ app/Http/Controllers/Admin/Dashboard/IndexController.php
@@ -40,14 +40,27 @@ class IndexController extends Controller
     public function index()
     {
         
-          $chart = new ChartJs;
+          $chart = new Fusioncharts;
         //   $chart->title("First Response Time");  
           $chart->labels(['January', 'February', 'March', 'April', 'May','June','July','August','September','Octobor','November','December']);
-          $chart->dataset('My dataset', 'pie', [5, 25, 3, 10,25,5, 25, 3, 10,25,10,20]);
+          $chart->dataset('My dataset', 'line', [5, 25, 3, 10,25,5, 25, 3, 10,25,10,20]);
           //Color For ChartJs
          // $chart->dataset('My dataset', 'bar', [5, 25, 3, 10,25,5, 25, 3, 10,25,10,20])->backgroundcolor('green')->color(collect(['#7d5fff','#32ff7e', '#ff4d4d']));
-   	
-         return view('admin.dashboard.index', ['chart' => $chart]);
+       
+         
+           $bar = new Fusioncharts;
+           $bar->labels(['January', 'February', 'March', 'April', 'May','June','July','August','September','Octobor','November','December']);
+           $bar->dataset('My Graph', 'bar', [5, 25, 3, 10,25,5, 25, 3, 10,25,10,20]);
+
+           $pie = new Frappe;
+           $pie->labels(['January', 'February', 'March', 'April', 'May']);
+           $pie->dataset('My Graph', 'pie', [5, 25, 3, 10,25]);
+
+         return view('admin.dashboard.index', [
+                                'chart' => $chart,
+                                'bar' => $bar,
+                                'pie' => $pie,
+                    ]);
     }
 
     public function profile() {
